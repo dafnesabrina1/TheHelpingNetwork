ee6c2f1a8fe7e6e132a9b1b18e060df05d712a94
Sebastian Crossa <crossasebastian@gmail.com>
2019-08-24 18:03:24 -0500

    Added base for project

---
+++ src/containers/Login/HeroSection/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeroSection = () => {
+  return (
+    <div>
+      <h1>This is the hero section</h1>
+    </div>
+  );
+};
+
+export default HeroSection;
---
+++ src/index.js
@@ -0,0 +1,28 @@
+// Libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import theme from './utils/theme';
+import GlobalStyles from './utils/global';
+
+import App from './App';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <>
+        <App />
+        <GlobalStyles />
+      </>
+    </ThemeProvider>
+  </BrowserRouter>,
+
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
