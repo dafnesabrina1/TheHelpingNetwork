4fe58bd9ab0286fa38e15f9c618226bd6a948b05
Dafne Sabrina Medina Garcia <dafne@Dafnes-MacBook-Pro.local>
2019-08-25 00:56:54 -0500

    Carousel

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,5 +1,7 @@
 // Libraries
 import React from 'react';
+import './Events.css';
+import './functionality';
 
 // Styles
 import EventCard from '../containers/Events/EventCard';
@@ -7,7 +9,63 @@ import EventCard from '../containers/Events/EventCard';
 const Events = () => {
   return (
     <div>
-      <EventCard />
+      <div class="MultiCarousel" data-items="1,3,5,6" data-slide="1" id="MultiCarousel"  data-interval="1000">
+            <div class="MultiCarousel-inner">
+                <div class="item">
+                  <EventCard/>
+                </div>
+                <div class="item">
+                  <EventCard/>  
+                </div>
+                <div class="item">
+                  <EventCard/>
+                </div>
+                <div class="item">
+                  <EventCard/>
+                </div>
+                <div class="item">
+                  <EventCard/>
+                </div>
+                <div class="item">
+                  <EventCard/>
+                </div>
+                <div class="item">
+                  <EventCard/>
+                </div>
+                <div class="item">
+                  <EventCard/>
+                </div>
+                <div class="item">
+                  <EventCard/>
+                </div>
+                <div class="item">
+                  <EventCard/>
+                </div>
+                <div class="item">
+                  <EventCard/>
+                </div>
+                <div class="item">
+                  <EventCard/>
+                </div>
+                <div class="item">
+                  <EventCard/>
+                </div>
+                <div class="item">
+                  <EventCard/>
+                </div>
+                <div class="item">
+                  <EventCard/>
+                </div>
+                <div class="item">
+                  <EventCard/>
+                </div>
+            </div>
+            <button class="btn btn-primary leftLst">.</button>
+            <button class="btn btn-primary rightLst">.</button>
+        </div>
+        <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css"/>
+        <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
     </div>
   );
 };
